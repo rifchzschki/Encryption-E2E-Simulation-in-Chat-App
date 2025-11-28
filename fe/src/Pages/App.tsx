70c03ded5775a6d20a2cc5d1c387bfeb62031332
Rici Trisna Putra <13522026@std.stei.itb.ac.id>
2025-11-29 02:07:57 +0700

    feat: merge contacts to app

--- fe/src/Pages/App.tsx
+++ fe/src/Pages/App.tsx
@@ -1,19 +1,17 @@
-import Navbar from '../components/Navbar';
 import ChatComponent from '../components/ChatComponent';
 import {useAuth} from '../context/AuthContext';
+import Contacts from '../components/Contacts'; // Adjust the path if necessary
 function App() {
   const {token, username} = useAuth();
   return (
     <>
-      <main className="flex bg-amber-200 h-screen">
-        <section className="bg-amber-700 w-1/3">
-          <Navbar />
-          <section>Search Contact</section>
+      <main className="flex h-screen">
+        <section className=" w-1/3">
+          <Contacts />
           <p>{token}</p>
           <p>{username}</p>
-          <section>Contact List</section>
         </section>
-        <section className="bg-amber-500 w-2/3">
+        <section className=" w-2/3">
           {/* <section>Profile Receiver Information</section> */}
           <section>
             <ChatComponent />
---
+++ fe/src/components/Contacts.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { List } from '@mui/material';
+import ContactList from '../components/ContactList';
+import ContactHeader from '../components/ContactHeader';
+
+function Contacts() {
+  const [showSearch, setShowSearch] = useState(false); // state toggle search bar
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const contacts = [
+    { name: 'Contact 1', avatar: 'https://i.pravatar.cc/40?img=1' },
+    { name: 'Contact 2', avatar: 'https://i.pravatar.cc/40?img=2' },
+    { name: 'Contact 3', avatar: 'https://i.pravatar.cc/40?img=3' },
+  ];
+
+  // filter contacts based on searchQuery
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className="">
+      {/* Contacts Sidebar */}
+      <div className="flex flex-col border-r border-blue-300">
+        {/* Contact Header with Search */}
+        <ContactHeader
+          showSearch={showSearch}
+          setShowSearch={setShowSearch}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+
+        {/* Contacts List */}
+        <div className="flex-grow overflow-y-auto">
+          <List className="p-0">
+            {filteredContacts.map((contact, index) => (
+              <ContactList
+                key={contact.name}
+                filteredContacts={filteredContacts}
+                contact={contact}
+                index={index}
+              />
+            ))}
+          </List>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Contacts;
