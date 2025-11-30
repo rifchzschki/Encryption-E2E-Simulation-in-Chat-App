de71e75911dad499019c79793d2f3764016611ae
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2025-11-30 18:00:03 +0700

    Update allowed origins in CORS middleware

--- be/middleware/CORS.go
+++ be/middleware/CORS.go
@@ -10,7 +10,7 @@ func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		allowedOrigins := map[string]bool{
 			"http://localhost:5173": true,
-			"https://encryption-e2e-simulation-in-chat-app-production.up.railway.app": true,
+			"https://encryption-e2-e-simulation-in-chat.vercel.app": true,
 		}
 
 		origin := c.Request.Header.Get("Origin")
