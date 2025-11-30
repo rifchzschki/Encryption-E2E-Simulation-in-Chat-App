86cdbdad4d2eec1a491537b8b97e6e7fedd6ef13
rifchzschki <rifkiqizara23@gmail.com>
2025-11-30 17:25:58 +0700

    feat: update cors to new deployed backend url

--- be/middleware/CORS.go
+++ be/middleware/CORS.go
@@ -8,7 +8,19 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		allowedOrigins := map[string]bool{
+			"http://localhost:5173": true,
+			"https://encryption-e2e-simulation-in-chat-app-production.up.railway.app": true,
+		}
+
+		origin := c.Request.Header.Get("Origin")
+        if allowedOrigins[origin] {
+            c.Header("Access-Control-Allow-Origin", origin)
+            c.Header("Vary", "Origin")
+        }
+
 		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Header("Access-Control-Allow-Origin", "http://encryption-e2e-simulation-in-chat-app-production.up.railway.app")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		c.Header("Access-Control-Expose-Headers", "Authorization")
