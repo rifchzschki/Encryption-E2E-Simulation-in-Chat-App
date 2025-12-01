08850838128a96ec0b98ae5b29e94ce08e691fa0
rifchzschki <rifkiqizara23@gmail.com>
2025-12-01 19:24:54 +0700

    fix: ui for showing latest messages each contact

--- fe/src/components/ContactComponent.tsx
+++ fe/src/components/ContactComponent.tsx
@@ -45,21 +45,22 @@ export default function ContactComponent({ contact }: ContactListProps) {
             />
           </ListItemAvatar>
 
-          <div className="flex justify-between w-full ml-2">
-            <div className="min-w-0">
+          <div className="flex justify-between w-full gap-2">
+            <div className="flex-1 min-w-0">
+              {' '}
               <Typography className="font-semibold truncate">
                 {contact.username}
               </Typography>
-              <Typography className="text-gray-500 text-sm truncate max-w-9/10 ">
+              <Typography className="text-gray-500 text-sm truncate max-w-full">
                 {meta?.latestMessage || 'Start chatting'}
               </Typography>
             </div>
 
-            <div className="flex flex-col items-end">
+            <div className="flex flex-col items-end shrink-0 ml-2">
+              {' '}
               <Typography variant="caption" className="text-gray-400">
                 {formattedTime}
               </Typography>
-
               {meta?.unreadCount > 0 && (
                 <div className="bg-blue-500 text-white rounded-full w-5 h-5 text-xs flex items-center justify-center mt-1 shadow-md">
                   {meta.unreadCount}
